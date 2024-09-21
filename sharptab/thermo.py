4aca64a38f29a2d86dc003e9c29bfbe83069c899
lcarlaw <leecarlaw@gmail.com>
2024-09-20 21:34:54 -0500

    Address significant error involving most-unstable parcel and effective inflow calculations due to unexpected behavior within try-catch block which was supposed to differentiate between scalars and arrays. Else block would be the only part of wobf() ever to execute, regardless of the sign of array t. Remove try statement to force proper evaluation depending on t above or below 0 C. Mainly an issue for colder/drier profiles. Alter compute.py to force jitted functions to recompile.

--- sharptab/thermo.py
+++ sharptab/thermo.py
@@ -780,62 +780,30 @@ def wobf(t):
     Correction to theta (C) for calculation of saturated potential temperature.
     """
     t = t - 20
-    try:
-        # If t is a scalar
-        if t <= 0:
-            npol = 1.0 + t * (
-                -8.841660499999999e-3
-                + t
-                * (
-                    1.4714143e-4
-                    + t
-                    * (
-                        -9.671989000000001e-7
-                        + t * (-3.2607217e-8 + t * (-3.8598073e-10))
-                    )
-                )
-            )
-            npol = 15.13 / (np.power(npol, 4))
-            return npol
-        else:
-            ppol = t * (
-                4.9618922e-07
-                + t
-                * (
-                    -6.1059365e-09
-                    + t * (3.9401551e-11 + t * (-1.2588129e-13 + t * (1.6688280e-16)))
-                )
-            )
-            ppol = 1 + t * (3.6182989e-03 + t * (-1.3603273e-05 + ppol))
-            ppol = (29.93 / np.power(ppol, 4)) + (0.96 * t) - 14.8
-            return ppol
-
-    except:
-        # If t is an array
-        npol = 1.0 + t * (
-            -8.841660499999999e-3
+    npol = 1.0 + t * (
+        -8.841660499999999e-3
+        + t
+        * (
+            1.4714143e-4
             + t
-            * (
-                1.4714143e-4
-                + t
-                * (-9.671989000000001e-7 + t * (-3.2607217e-8 + t * (-3.8598073e-10)))
-            )
+            * (-9.671989000000001e-7 + t * (-3.2607217e-8 + t * (-3.8598073e-10)))
         )
-        npol = 15.13 / (np.power(npol, 4))
-        ppol = t * (
-            4.9618922e-07
-            + t
-            * (
-                -6.1059365e-09
-                + t * (3.9401551e-11 + t * (-1.2588129e-13 + t * (1.6688280e-16)))
-            )
+    )
+    npol = 15.13 / (np.power(npol, 4))
+    ppol = t * (
+        4.9618922e-07
+        + t
+        * (
+            -6.1059365e-09
+            + t * (3.9401551e-11 + t * (-1.2588129e-13 + t * (1.6688280e-16)))
         )
-        ppol = 1 + t * (3.6182989e-03 + t * (-1.3603273e-05 + ppol))
-        ppol = (29.93 / np.power(ppol, 4)) + (0.96 * t) - 14.8
-        correction = np.zeros(t.shape, dtype=np.float64)
-        correction[t <= 0] = npol[t <= 0]
-        correction[t > 0] = ppol[t > 0]
-        return correction
+    )
+    ppol = 1 + t * (3.6182989e-03 + t * (-1.3603273e-05 + ppol))
+    ppol = (29.93 / np.power(ppol, 4)) + (0.96 * t) - 14.8
+    correction = np.zeros(t.shape, dtype=np.float64)
+    correction[t <= 0] = npol[t <= 0]
+    correction[t > 0] = ppol[t > 0]
+    return correction
 
 @njit
 def satlift(p, thetam, conv=0.1):
